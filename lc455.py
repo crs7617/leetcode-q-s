bae2a4c80e48780315a734863d066cd361428dd7
C R SAIRAM <115174268+crs7617@users.noreply.github.com>
2024-01-01 23:02:43 +0530

    Create lc455.py

---
+++ lc455.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findContentChildren(self, g, s):
+        g.sort()
+        s.sort()
+        i=j=0
+
+        while i<len(g):
+            while j<len(s) and g[i]>s[j]:
+                j+=1
+            if j==len(s):
+                break
+            
+            i+=1
+            j+=1
+        return i
+        
