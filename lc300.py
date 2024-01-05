33cc8c57029b75ce859a776895644ef3595c5d1b
C R SAIRAM <115174268+crs7617@users.noreply.github.com>
2024-01-05 20:15:51 +0530

    Create lc300.py

---
+++ lc300.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        l=[1]*len(nums)
+
+        for i in range(len(nums)-1,-1,-1):
+            for j in range(i+1,len(nums)):
+                if nums[i]<nums[j]:
+                    l[i]=max(l[i],1+l[j])
+        return max(l)
+        
