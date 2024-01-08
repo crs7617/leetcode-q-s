2e03fa393be7d34da3aa0dee8f25a2f6acfe87d5
C R SAIRAM <115174268+crs7617@users.noreply.github.com>
2024-01-08 19:37:33 +0530

    Create lc938.py

---
+++ lc938.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def rangeSumBST(self, root, low, high):
+
+        if not root:
+            return 0
+        
+        if root.val>high:
+            return self.rangeSumBST(root.left,low,high)
+        
+        if root.val<low:
+            return self.rangeSumBST(root.right,low,high)
+
+        return root.val+ self.rangeSumBST(root.left,low,high)+ self.rangeSumBST(root.right,low,high)
+
+
+        
